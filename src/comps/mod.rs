c6de08d816368e98eb8bb8364bd134149a800a71
ggymm <18842652126@163.com>
2025-10-10 10:10:33 +0800

    update

--- src/comps/mod.rs
+++ src/comps/mod.rs
@@ -2,7 +2,14 @@ use gpui::*;
 use gpui_component::input::{InputState, TextInput};
 
 pub fn page() -> Div {
-    div().flex().flex_col().p_5().gap_5().bg(rgb(0x282828))
+    div()
+        .flex()
+        .flex_col()
+        .p_5()
+        .gap_5()
+        .min_w_0()
+        .min_h_0()
+        .bg(rgb(0x282828))
 }
 
 pub fn card() -> Div {
@@ -21,6 +28,14 @@ pub fn label(title: impl IntoElement) -> Div {
     div().text_sm().text_color(white()).child(title)
 }
 
+pub fn input(state: &Entity<InputState>) -> TextInput {
+    TextInput::new(state)
+        .h_full()
+        .appearance(false)
+        .focus_bordered(false)
+        .text_color(white())
+}
+
 pub fn textarea(
     state: &Entity<InputState>,
     config: impl FnOnce(TextInput) -> TextInput,
@@ -33,12 +48,4 @@ pub fn textarea(
         .border_color(rgb(0x404040))
         .rounded_lg()
         .child(config(input(state)))
-}
-
-fn input(state: &Entity<InputState>) -> TextInput {
-    TextInput::new(state)
-        .h_full()
-        .appearance(false)
-        .focus_bordered(false)
-        .text_color(white())
 }
